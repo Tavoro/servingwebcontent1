77c7b21674fca66ea1c2bd1d23ba4632b658066b
Tavorot <kubatan77@yandex.ru>
2020-01-20 18:16:25 +0500

    asd

--- src/main/java/com/example/servingwebcontent/Models/User.java
+++ src/main/java/com/example/servingwebcontent/Models/User.java
@@ -1,15 +1,27 @@
-package com.example.servingwebcontent;
+package com.example.servingwebcontent.Models;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 
 public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
     private String name;
     private String pass;
 
     public User() {
     }
 
-    public User(String name, String pass) {
+    public User(String name, String pass, Integer id) {
         this.name = name;
         this.pass = pass;
+        this.id = id;
     }
 
     public String getName() {
@@ -28,11 +40,20 @@ public class User {
         this.pass = pass;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
                 ", pass='" + pass + '\'' +
+                ", id=" + id +
                 '}';
     }
 }
